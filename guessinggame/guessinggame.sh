b9462ddc391fcd48339043ca54d9349a7d3a22db
Vankisa <vankisa@gmail.com>
2017-09-14 10:53:36 +0200

    debugging and added code for non-integer inputs

--- guessinggame/guessinggame.sh
+++ guessinggame/guessinggame.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 # File: guessinggame.sh
 
 function ask(){
@@ -8,20 +8,26 @@ function ask(){
 	do
 		echo "How many files are there in your current directory?"
 		read response
-		if [ $response -gt $cnt_files]
+		if [ $response -eq $response 2>/dev/null ]
 		then
-			echo "Your guess is higher than the correct answer"
-			echo "Please guess again"
-		elif [ $response -lt $cnt_files ]
-		then
-			echo "Your guess is lower than the correct answer"
-			echo "Please guess again"
+			if [ $response -gt $cnt_files ]
+			then
+				echo "Your guess is higher than the correct answer"
+				echo "Please guess again"
+			elif [ $response -lt $cnt_files ]
+			then
+				echo "Your guess is lower than the correct answer"
+				echo "Please guess again"
+			else
+				echo "Your answer was correct. Congratilations!"
+				break
+			fi
 		else
-			echo "Your answer was correct. Congratilations!"
-		break
+			echo "$response is not an integer."
+			echo "Please provide an integer as a guess"
 		fi
 	done
 }
 echo "Welcome to the guessing game!"
 echo "Try to guess the correct answer."
-ask()
+ask
